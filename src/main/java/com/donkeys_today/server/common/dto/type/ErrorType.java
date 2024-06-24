d3967dee96457953cd1700ab80f6fdfd5db554d6
hyunw9 <rkdgusdnr32@naver.com>
2024-06-24 23:24:57 +0900

    Feat : 성공/실패 Enum 정의

---
+++ src/main/java/com/donkeys_today/server/common/dto/type/ErrorType.java
@@ -0,0 +1,22 @@
+package com.donkeys_today.server.common.dto.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorType {
+
+  /**
+   * 인증 / 인가 관련 오류
+   */
+  JWT_UNAUTHORIZED_EXCEPTION(HttpStatus.UNAUTHORIZED.value(), "사용자의 로그인 검증을 실패했습니다."),
+  INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED.value(), "올바르지 않은 리프레시 토큰입니다."),
+  EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED.value(), "리프레시 토큰이 만료되었습니다. 다시 로그인해 주세요."),
+  INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED.value(), "올바르지 않은 액세스 토큰입니다.")
+  ;
+  private final int status;
+  private final String message;
+
+}
---
+++ src/main/java/com/donkeys_today/server/common/dto/type/SuccessType.java
@@ -0,0 +1,16 @@
+package com.donkeys_today.server.common.dto.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum SuccessType {
+
+  CREATED_SUCCESS(HttpStatus.CREATED.value(), "생성이 완료되었습니다."),
+  OK_SUCCESS(HttpStatus.OK.value(), "정상 요청 완료."),;
+
+  private final int status;
+  private final String message;
+}
