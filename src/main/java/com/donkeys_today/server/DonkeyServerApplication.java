6bf6ca70d2dff24832a58e3de00664d573231bb1
hyunw9 <rkdgusdnr32@naver.com>
2024-06-24 23:24:17 +0900

    Feat : 초기 실행 파일

---
+++ src/main/java/com/donkeys_today/server/DonkeyServerApplication.java
@@ -0,0 +1,13 @@
+package com.donkeys_today.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonkeyServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DonkeyServerApplication.class, args);
+  }
+
+}
