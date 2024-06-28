81ca9cee3565b0783626d5069e8194d0513f43b3
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-06-28 23:14:31 +0900

    Feat: Spring Security Chain 을 설정합니다. (#8)
    * Feat: 스프링 시큐리티 추가
    
    * Feat: security chain 세팅
    
    * Style: 코드 정렬

---
+++ src/main/java/com/donkeys_today/server/auth/config/WhiteListConstants.java
@@ -0,0 +1,23 @@
+package com.donkeys_today.server.auth.config;
+
+import java.util.List;
+
+public class WhiteListConstants {
+    public static final List<String> FILTER_WHITE_LIST = List.of(
+            "/login/oauth2/code/kakao",
+            "/login/oauth2/code/google",
+            "/api/v1",
+            "/login",
+            "/oauth/authorize",
+            "/actuator/health",
+            "/error",
+            "/swagger-ui/",
+            "/swagger-resources/",
+            "/api-docs/"
+    );
+
+    public static final String[] SECURITY_WHITE_LIST = {
+            "/login/**",
+            "/",
+    };
+}
