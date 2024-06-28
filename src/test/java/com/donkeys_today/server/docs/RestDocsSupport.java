0f43113fdc020390eec4cec43b8bc70d7a528130
hyunw9 <rkdgusdnr32@naver.com>
2024-06-28 10:43:23 +0900

    Feat : RestAssuredMockMVC 의존성 추가

--- src/test/java/com/donkeys_today/server/docs/RestDocsSupport.java
+++ src/test/java/com/donkeys_today/server/docs/RestDocsSupport.java
@@ -3,6 +3,7 @@ package com.donkeys_today.server.docs;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.restdocs.RestDocumentationContextProvider;
@@ -21,6 +22,7 @@ public abstract class RestDocsSupport {
     this.mockMvc = MockMvcBuilders.standaloneSetup(initController())
         .apply(documentationConfiguration(provider))
         .build();
+    RestAssuredMockMvc.mockMvc(this.mockMvc);
   }
 
   protected abstract Object initController() throws Exception;
