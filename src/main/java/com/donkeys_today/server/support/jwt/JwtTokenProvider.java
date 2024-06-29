0eae83617dd70ab5dd7d941e6f4e4a6e893526e3
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2024-06-29 15:03:49 +0900

    Refactor: JWT token 관련 작업을 수행하는 JWTUtils 컴포넌트 생성

--- src/main/java/com/donkeys_today/server/support/jwt/JwtTokenProvider.java
+++ src/main/java/com/donkeys_today/server/support/jwt/JwtTokenProvider.java
@@ -1,19 +1,9 @@
 package com.donkeys_today.server.support.jwt;
 
-import static com.donkeys_today.server.support.dto.type.ErrorType.EXPIRED_TOKEN;
-import static com.donkeys_today.server.support.dto.type.ErrorType.INVALID_TOKEN;
-import static com.donkeys_today.server.support.dto.type.ErrorType.UNKNOWN_TOKEN;
-import static com.donkeys_today.server.support.dto.type.ErrorType.UNSUPPORTED_TOKEN;
-import static com.donkeys_today.server.support.dto.type.ErrorType.WRONG_SIGNATURE_TOKEN;
-import com.donkeys_today.server.support.exception.UnauthorizedException;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
 import javax.crypto.SecretKey;
@@ -57,48 +47,5 @@ public class JwtTokenProvider {
         return Keys.hmacShaKeyFor(
                 encodedKey.getBytes());
     }
-    public JwtValidationType validateToken(String token) {
-        try {
-            final Claims claims = getBody(token);
-            if (claims.get(JWTConstants.TOKEN_TYPE).toString().equals(JWTConstants.ACCESS_TOKEN)) {
-                return JwtValidationType.VALID_ACCESS;
-            } else if (claims.get(JWTConstants.TOKEN_TYPE).toString().equals(JWTConstants.REFRESH_TOKEN)) {
-                return JwtValidationType.VALID_REFRESH;
-            }
-            throw new UnauthorizedException(INVALID_TOKEN);
-        } catch (MalformedJwtException e) {
-            throw new UnauthorizedException(INVALID_TOKEN);
-        } catch (ExpiredJwtException e) {
-            throw new UnauthorizedException(EXPIRED_TOKEN);
-        } catch (IllegalArgumentException e) {
-            throw new UnauthorizedException(UNKNOWN_TOKEN);
-        } catch (UnsupportedJwtException e) {
-            throw new UnauthorizedException(UNSUPPORTED_TOKEN);
-        } catch (SignatureException e) {
-            throw new UnauthorizedException(WRONG_SIGNATURE_TOKEN);
-        }
-    }
 
-    private Claims getBody(final String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
-    public String getUserFromJwt(String token) {
-        Claims claims = getBody(token);
-        return claims.get(JWTConstants.USER_ID).toString();
-    }
-
-    public String getRoleFromJwt(String token) {
-        Claims claims = getBody(token);
-        return claims.get(JWTConstants.ROLE).toString();
-    }
-
-    public String getTypeFromJwt(String token) {
-        Claims claims = getBody(token);
-        return claims.get(JWTConstants.TOKEN_TYPE).toString();
-    }
 }
--- src/test/java/com/donkeys_today/server/docs/JwtTokenProviderTest.java
+++ src/test/java/com/donkeys_today/server/docs/JwtTokenProviderTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import com.donkeys_today.server.support.exception.UnauthorizedException;
 import com.donkeys_today.server.support.jwt.JWTConstants;
+import com.donkeys_today.server.support.jwt.JWTUtil;
 import com.donkeys_today.server.support.jwt.JwtTokenProvider;
 import com.donkeys_today.server.support.jwt.JwtValidationType;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +17,9 @@ public class JwtTokenProviderTest {
 
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private JWTUtil jwtUtil;
 
     @Test
     @DisplayName("액세스 토큰 발급 테스트")
@@ -49,7 +53,7 @@ public class JwtTokenProviderTest {
     @DisplayName("JWT에서 사용자 ID 가져오기 테스트")
     public void testGetUserFromJwt() {
         String token = jwtTokenProvider.issueAccessToken(1L, "USER");
-        String userId = jwtTokenProvider.getUserFromJwt(token);
+        String userId = jwtUtil.getUserFromJwt(token);
         assertEquals("1", userId);
     }
 
@@ -57,7 +61,7 @@ public class JwtTokenProviderTest {
     @DisplayName("JWT에서 역할 가져오기 테스트")
     public void testGetRoleFromJwt() {
         String token = jwtTokenProvider.issueAccessToken(1L, "USER");
-        String role = jwtTokenProvider.getRoleFromJwt(token);
+        String role = jwtUtil.getRoleFromJwt(token);
         assertEquals("USER", role);
     }
 
@@ -65,7 +69,7 @@ public class JwtTokenProviderTest {
     @DisplayName("JWT에서 유형 가져오기 테스트")
     public void testGetTypeFromJwt() {
         String token = jwtTokenProvider.issueAccessToken(1L, "USER");
-        String type = jwtTokenProvider.getTypeFromJwt(token);
+        String type = jwtUtil.getTypeFromJwt(token);
         assertEquals(JWTConstants.ACCESS_TOKEN, type);
     }
 
@@ -73,13 +77,13 @@ public class JwtTokenProviderTest {
     @DisplayName("토큰 유효성 검사 - 유효한 액세스 토큰 테스트")
     public void testValidateToken_ValidAccessToken() {
         String token = jwtTokenProvider.issueAccessToken(1L, "USER");
-        JwtValidationType validationType = jwtTokenProvider.validateToken(token);
+        JwtValidationType validationType = jwtUtil.validateToken(token);
         assertEquals(JwtValidationType.VALID_ACCESS, validationType);
     }
 
     @Test
     @DisplayName("토큰 유효성 검사 - 잘못된 토큰 테스트")
     public void testValidateToken_InvalidToken() {
-        assertThrows(UnauthorizedException.class, () -> jwtTokenProvider.validateToken("invalidtoken"));
+        assertThrows(UnauthorizedException.class, () -> jwtUtil.validateToken("invalidtoken"));
     }
 }
