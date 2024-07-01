42c6096371123086c72b4dedc0928fadc1308931
hyunw9 <rkdgusdnr32@naver.com>
2024-07-01 22:21:01 +0900

    Chore : Profile 설정

--- src/main/java/com/donkeys_today/server/DonkeyServerApplication.java
+++ src/main/java/com/donkeys_today/server/DonkeyServerApplication.java
@@ -2,8 +2,10 @@ package com.donkeys_today.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("local")
 public class DonkeyServerApplication {
 
   public static void main(String[] args) {
