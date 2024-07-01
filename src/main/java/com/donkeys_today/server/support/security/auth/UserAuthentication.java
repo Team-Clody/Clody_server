06721270d4a33f6418ada9a5bdba389edbe283fc
hyunw9 <rkdgusdnr32@naver.com>
2024-07-01 22:17:32 +0900

    Feat : UserAuthentication 구현

---
+++ src/main/java/com/donkeys_today/server/support/security/auth/UserAuthentication.java
@@ -0,0 +1,17 @@
+package com.donkeys_today.server.support.security.auth;
+
+import java.util.Collection;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+public class UserAuthentication extends UsernamePasswordAuthenticationToken {
+
+  public UserAuthentication(Object principal, Object credentials,
+      Collection<? extends GrantedAuthority> authorities) {
+    super(principal, credentials, authorities);
+  }
+
+  public static UserAuthentication createUserAuthentication(Long userId){
+    return new UserAuthentication(userId,null,null);
+  }
+}
