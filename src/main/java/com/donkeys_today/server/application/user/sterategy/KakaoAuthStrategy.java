19d35ac7ad1b2ff24caab4f7a51a2b3daaed7a70
hyunw9 <rkdgusdnr32@naver.com>
2024-07-04 01:22:04 +0900

    Feat : Login 구현

--- src/main/java/com/donkeys_today/server/application/user/sterategy/KakaoAuthStrategy.java
+++ src/main/java/com/donkeys_today/server/application/user/sterategy/KakaoAuthStrategy.java
@@ -5,6 +5,7 @@ import com.donkeys_today.server.domain.user.User;
 import com.donkeys_today.server.domain.user.UserRepository;
 import com.donkeys_today.server.support.dto.type.ErrorType;
 import com.donkeys_today.server.support.exception.BusinessException;
+import com.donkeys_today.server.support.exception.NotFoundException;
 import com.donkeys_today.server.support.feign.dto.response.KakaoTokenResponse;
 import com.donkeys_today.server.support.feign.dto.response.KakaoUserInfoResponse;
 import com.donkeys_today.server.support.feign.kakao.KakaoAuthClient;
@@ -15,62 +16,64 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class KakaoAuthStrategy implements UserAuthSterategy {
+public class KakaoAuthStrategy implements SocialRegisterSterategy {
 
-    private final KakaoAuthClient kakaoAuthClient;
-    private final KakaoUserInfoClient kakaoUserInfoClient;
-    private final UserRepository userRepository;
+  private final KakaoAuthClient kakaoAuthClient;
+  private final KakaoUserInfoClient kakaoUserInfoClient;
+  private final UserRepository userRepository;
 
-    @Value("${kakao.client-id}")
-    private String kakaoClientId;
-    @Value("${kakao.client-secret}")
-    private String kakaoClientSecret;
-    @Value("${kakao.redirect-uri}")
-    private String redirect_uri;
+  @Value("${kakao.client-id}")
+  private String kakaoClientId;
+  @Value("${kakao.client-secret}")
+  private String kakaoClientSecret;
+  @Value("${kakao.redirect-uri}")
+  private String redirect_uri;
 
-    @Override
-    public User signUp(String authToken) {
-        
-        KakaoTokenResponse token = getKakaoToken(authToken);
-        String accessTokenWithPrefix = KakaoTokenResponse.getTokenWithPrefix(token.access_token());
-        KakaoUserInfoResponse userInfo = getKakaoUserInfo(accessTokenWithPrefix);
-        validateDuplicateUser(userInfo);
-        return User.builder()
-                .platformID(userInfo.id())
-                .nickName(userInfo.kakaoAccount().name())
-                .email(userInfo.kakaoAccount().email())
-                .build();
+  @Override
+  public User signUp(Platform platform, String authToken) {
+    KakaoTokenResponse token = getKakaoToken(authToken);
+    String accessTokenWithPrefix = KakaoTokenResponse.getTokenWithPrefix(token.access_token());
+    KakaoUserInfoResponse userInfo = getKakaoUserInfo(accessTokenWithPrefix);
+    validateDuplicateUser(userInfo);
+    return User.builder()
+        .platformID(userInfo.id())
+        .platform(platform)
+        .nickName(userInfo.kakaoAccount().name())
+        .email(userInfo.kakaoAccount().email())
+        .build();
+  }
+
+  @Override
+  public User signIn(Platform platform, String authToken) {
+    KakaoTokenResponse token = getKakaoToken(authToken);
+    String accessTokenWithPrefix = KakaoTokenResponse.getTokenWithPrefix(token.access_token());
+    KakaoUserInfoResponse userInfo = getKakaoUserInfo(accessTokenWithPrefix);
+    return findByPlatformAndPlatformId(platform,userInfo.id());
+  }
+
+  private User findByPlatformAndPlatformId(Platform platform, Long platformId) {
+    return userRepository.findByPlatformAndPlatformID(platform, platformId).orElseThrow(
+        () -> new NotFoundException(ErrorType.DUPLICATED_USER_ERROR)
+    );
+  }
+
+  private KakaoTokenResponse getKakaoToken(String authToken) {
+    return kakaoAuthClient.getOauth2AccessToken(
+        "authorization_code",
+        kakaoClientId,
+        kakaoClientSecret,
+        redirect_uri,
+        authToken
+    );
+  }
+
+  private KakaoUserInfoResponse getKakaoUserInfo(String accessToken) {
+    return kakaoUserInfoClient.getUserInformation(accessToken);
+  }
+
+  private void validateDuplicateUser(KakaoUserInfoResponse userInfo) {
+    if (userRepository.existsByPlatformAndPlatformID(Platform.KAKAO, userInfo.id())) {
+      throw new BusinessException(ErrorType.DUPLICATED_USER_ERROR);
     }
-
-    @Override
-    public User signIn(String authToken) {
-        return null;
-    }
-
-    private KakaoTokenResponse getKakaoToken(String authToken) {
-
-        return kakaoAuthClient.getOauth2AccessToken(
-                "authorization_code",
-                kakaoClientId,
-                kakaoClientSecret,
-                redirect_uri,
-                authToken
-        );
-    }
-
-    private KakaoUserInfoResponse getKakaoUserInfo(String accessToken) {
-
-        return kakaoUserInfoClient.getUserInformation(accessToken);
-    }
-
-    private void validateDuplicateUser(KakaoUserInfoResponse userInfo) {
-        if (userRepository.existsByPlatformAndPlatformID(Platform.KAKAO, userInfo.id())) {
-            throw new BusinessException(ErrorType.DUPLICATED_USER_ERROR);
-        }
-    }
-
-    public String getRedirectUri() {
-        return String.format("%s?client_id=%s&redirect_uri=%s&response_type=code",
-                "https://kauth.kakao.com/oauth/authorize", kakaoClientId, redirect_uri);
-    }
+  }
 }
