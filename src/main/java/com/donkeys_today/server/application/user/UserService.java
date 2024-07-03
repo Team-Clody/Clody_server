1b220cbee8e0ee036fc8b5ede437a23fefe22739
hyunw9 <rkdgusdnr32@naver.com>
2024-07-03 19:23:17 +0900

    Feat : 인증 관련 비즈니스 로직 구현

--- src/main/java/com/donkeys_today/server/application/user/UserService.java
+++ src/main/java/com/donkeys_today/server/application/user/UserService.java
@@ -1,31 +1,51 @@
 package com.donkeys_today.server.application.user;
 
-import com.donkeys_today.server.application.user.dto.request.UserSignUpRequest;
 import com.donkeys_today.server.domain.user.Platform;
+import com.donkeys_today.server.domain.user.User;
 import com.donkeys_today.server.domain.user.UserRepository;
-import com.donkeys_today.server.support.feign.kakao.KakaoAuthClient;
+import com.donkeys_today.server.presentation.user.dto.requset.UserSignUpRequest;
+import com.donkeys_today.server.presentation.user.dto.response.UserSignUpResponse;
 import com.donkeys_today.server.support.jwt.JwtProvider;
 import com.donkeys_today.server.support.jwt.RefreshTokenRepository;
+import com.donkeys_today.server.support.jwt.Token;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
   private final UserRepository userRepository;
-  private final UserCreater userCreater;
   private final UserRetriever userRetriever;
+  private final UserAuthenticator userAuthenticator;
   private final UserUpdater userUpdater;
   private final UserRemover userRemover;
 
   private final RefreshTokenRepository refreshTokenRepository;
   private final JwtProvider jwtProvider;
-  private final KakaoAuthClient kakaoAuthClient;
 
-  public UserRegisterResponse register(String token, UserSignUpRequest request){
-    Platform platform = get
-
+  @Transactional
+  public UserSignUpResponse signUp(final String authorizationCode,
+      final UserSignUpRequest request) {
+    //플랫폼에서 플랫폼 추출해야 함
+    //그래서 해당 플랫폼에다가 유저 생성 기능 위임
+    Platform platform = getPlatformFromRequestString(request.platform());
+    User newUser= userAuthenticator.signUp(authorizationCode, platform);
+    User savedUser = userRepository.save(newUser);
+    //알람 설정했을 경우, 알람 설정
+    userAuthenticator.setUserAlarm(savedUser,request.alarmAgreement(),request.alarmTime());
+    //토큰 생성하고 redis에 저장.
+    Token token = userAuthenticator.issueToken(savedUser.getId());
+    return UserSignUpResponse.of(savedUser.getId(), token.accessToken(),token.refreshToken());
   }
 
+  private Platform getPlatformFromRequestString(String request) {
+    return Platform.fromString(request);
+  }
+
+
+  public String redirect() {
+    return userAuthenticator.getKakaoRedirectRequest();
+  }
 }
