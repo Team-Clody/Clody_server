7ab9e4b0849052d49993d2bc959187de1a014f0a
hyunw9 <rkdgusdnr32@naver.com>
2024-07-04 01:24:31 +0900

    Feat : 서비스 코드 구현

--- src/main/java/com/donkeys_today/server/application/user/UserService.java
+++ src/main/java/com/donkeys_today/server/application/user/UserService.java
@@ -3,7 +3,9 @@ package com.donkeys_today.server.application.user;
 import com.donkeys_today.server.domain.user.Platform;
 import com.donkeys_today.server.domain.user.User;
 import com.donkeys_today.server.domain.user.UserRepository;
+import com.donkeys_today.server.presentation.user.dto.requset.UserSignInRequest;
 import com.donkeys_today.server.presentation.user.dto.requset.UserSignUpRequest;
+import com.donkeys_today.server.presentation.user.dto.response.UserSignInResponse;
 import com.donkeys_today.server.presentation.user.dto.response.UserSignUpResponse;
 import com.donkeys_today.server.support.jwt.JwtProvider;
 import com.donkeys_today.server.support.jwt.RefreshTokenRepository;
@@ -33,19 +35,23 @@ public class UserService {
     Platform platform = getPlatformFromRequestString(request.platform());
     User newUser= userAuthenticator.signUp(authorizationCode, platform);
     User savedUser = userRepository.save(newUser);
-    //알람 설정했을 경우, 알람 설정
+    //알람 설정했을 경우, 알람 설정 (이벤트 분리)
     userAuthenticator.setUserAlarm(savedUser,request.alarmAgreement(),request.alarmTime());
     //토큰 생성하고 redis에 저장.
     Token token = userAuthenticator.issueToken(savedUser.getId());
     return UserSignUpResponse.of(savedUser.getId(), token.accessToken(),token.refreshToken());
   }
 
-  private Platform getPlatformFromRequestString(String request) {
-    return Platform.fromString(request);
+  public UserSignInResponse signIn(final String authorizationCode,
+      final UserSignInRequest request){
+
+    Platform platform = getPlatformFromRequestString(request.platform());
+    User foundUser = userAuthenticator.signIn(authorizationCode,platform);
+    Token token = userAuthenticator.issueToken(foundUser.getId());
+    return UserSignInResponse.of(foundUser.getId(), token.accessToken(),token.refreshToken());
   }
 
-
-  public String redirect() {
-    return userAuthenticator.getKakaoRedirectRequest();
+  private Platform getPlatformFromRequestString(String request) {
+    return Platform.fromString(request);
   }
 }
