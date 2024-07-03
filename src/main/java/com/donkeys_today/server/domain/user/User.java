4224f67f7ab053c4befafac8d8ffd5c0081fc440
hyunw9 <rkdgusdnr32@naver.com>
2024-07-03 19:22:58 +0900

    Feat : Controller / Model / Repository

--- src/main/java/com/donkeys_today/server/domain/user/User.java
+++ src/main/java/com/donkeys_today/server/domain/user/User.java
@@ -6,11 +6,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
 
   @Id
@@ -18,6 +22,8 @@ public class User {
   private Long id;
 
   private Long platformID;
+
+  private Platform platform;
 
   @Column(name = "email")
   private String email;
@@ -34,8 +40,10 @@ public class User {
   private boolean is_deleted;
 
   @Builder
-  public User(String email, String userName, String nickName, boolean alarmAgreement,
-      boolean replyAgreement, boolean is_deleted) {
+  public User(Long platformID, Platform platform, String email, String userName, String nickName,
+      boolean alarmAgreement, boolean replyAgreement, boolean is_deleted) {
+    this.platformID = platformID;
+    this.platform = platform;
     this.email = email;
     this.userName = userName;
     this.nickName = nickName;
@@ -43,4 +51,9 @@ public class User {
     this.replyAgreement = replyAgreement;
     this.is_deleted = is_deleted;
   }
+
+  public void updateUserAlarmAgreement(boolean alarmAgreement) {
+    this.alarmAgreement = alarmAgreement;
+  }
+
 }
--- src/main/java/com/donkeys_today/server/presentation/user/UserController.java
+++ src/main/java/com/donkeys_today/server/presentation/user/UserController.java
@@ -1,17 +1,19 @@
 package com.donkeys_today.server.presentation.user;
 
 import com.donkeys_today.server.application.user.UserService;
-import com.donkeys_today.server.application.user.dto.request.UserSignUpRequest;
+import com.donkeys_today.server.common.constants.Constants;
+import com.donkeys_today.server.presentation.user.dto.requset.UserSignUpRequest;
+import com.donkeys_today.server.presentation.user.dto.response.UserSignUpResponse;
 import com.donkeys_today.server.support.dto.ApiResponse;
 import com.donkeys_today.server.support.dto.type.SuccessType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,16 +23,24 @@ public class UserController {
 
   private final UserService userService;
 
-  @PostMapping("/user/signUp")
-  public ResponseEntity<ApiResponse<?>> signUpUser(@RequestBody UserSignUpRequest request) {
-    return ResponseEntity.status(HttpStatus.CREATED)
-        .body(ApiResponse.success(SuccessType.CREATED_SUCCESS, userService.signUp(request)));
+  @RequestMapping("/auth/index")
+  public String main(Model model) {
+    return "kakao";
   }
 
-  @GetMapping("/user")
-  public ResponseEntity<ApiResponse<?>> getUserById(@RequestParam("id") long id) {
-    return ResponseEntity.status(HttpStatus.OK)
-        .body(ApiResponse.success(SuccessType.OK_SUCCESS,userService.getUser(id)));
+  @PostMapping("/auth/signUp")
+  public ResponseEntity<ApiResponse<?>> signUp(
+      @RequestHeader(Constants.AUTHORIZATION) final String authorization_code,
+      @RequestBody final UserSignUpRequest userSignUpRequest) {
+    final UserSignUpResponse response = userService.signUp(authorization_code,userSignUpRequest);
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(ApiResponse.success(SuccessType.CREATED_SUCCESS, response));
   }
+
+//  @GetMapping("/user")
+//  public ResponseEntity<ApiResponse<?>> getUserById(@RequestParam("id") long id) {
+//    return ResponseEntity.status(HttpStatus.OK)
+//        .body(ApiResponse.success(SuccessType.OK_SUCCESS, userService.getUser(id)));
+//  }
 
 }
