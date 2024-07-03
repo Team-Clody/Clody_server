6c9fe13b8f772cbe8f0547e7927fe9c491fa442f
hyunw9 <rkdgusdnr32@naver.com>
2024-07-03 19:21:22 +0900

    Feat : Constatns

--- src/main/java/com/donkeys_today/server/support/security/config/WhiteListConstants.java
+++ src/main/java/com/donkeys_today/server/support/security/config/WhiteListConstants.java
@@ -3,24 +3,25 @@ package com.donkeys_today.server.support.security.config;
 import java.util.List;
 
 public class WhiteListConstants {
-    public static final List<String> FILTER_WHITE_LIST = List.of(
-            "/login/oauth2/code/kakao",
-            "/login/oauth2/code/google",
-            "/api/v1",
-            "/login",
-            "/oauth/authorize",
-            "/actuator/health",
-            "/error",
-            "/swagger-ui/",
-            "/swagger-resources/",
-            "/api-docs/"
-    );
 
-    public static final String[] SECURITY_WHITE_LIST = {
-            "/login/**",
-            "/",
-            "/actuator/health",
-            "/api-docs/**",
-            "swagger-ui/**"
-    };
+  public static final List<String> FILTER_WHITE_LIST = List.of(
+      "/api/v1/auth/**",
+      "/",
+      "/api/v1/kakao",
+      "/oauth/authorize",
+      "/actuator/health",
+      "/error",
+      "/swagger-ui/",
+      "/swagger-resources/",
+      "/api-docs/"
+  );
+
+  public static final String[] SECURITY_WHITE_LIST = {
+      "/api/v1/kakao",
+      "/api/v1/auth/**",
+      "/",
+      "/actuator/health",
+      "/api-docs/**",
+      "swagger-ui/**"
+  };
 }
