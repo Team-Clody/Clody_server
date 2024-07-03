79e5f78f10d1e2d58bfd0c0c06201738014f15d4
hyunw9 <rkdgusdnr32@naver.com>
2024-07-03 19:23:49 +0900

    Feat : profile 삭제 -> 직접 설정 팔요

--- src/main/java/com/donkeys_today/server/DonkeyServerApplication.java
+++ src/main/java/com/donkeys_today/server/DonkeyServerApplication.java
@@ -2,10 +2,8 @@ package com.donkeys_today.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@Profile("local")
 public class DonkeyServerApplication {
 
   public static void main(String[] args) {
