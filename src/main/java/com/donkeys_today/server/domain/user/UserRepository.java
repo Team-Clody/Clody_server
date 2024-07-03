e2962ae8ca307fd93cc35b423f4f4eb14473f3a8
hyunw9 <rkdgusdnr32@naver.com>
2024-07-04 01:25:33 +0900

    Feat : Platform + Platform ID Search

--- src/main/java/com/donkeys_today/server/domain/user/UserRepository.java
+++ src/main/java/com/donkeys_today/server/domain/user/UserRepository.java
@@ -1,5 +1,6 @@
 package com.donkeys_today.server.domain.user;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
   boolean existsByPlatformAndPlatformID(Platform platform, Long platformID);
+  Optional<User> findByPlatformAndPlatformID(Platform platform, Long platformID);
 }
