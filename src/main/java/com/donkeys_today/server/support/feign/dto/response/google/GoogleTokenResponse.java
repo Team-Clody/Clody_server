636a594a7902766408d8a87fe74cbf820d926903
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-07-05 18:59:23 +0900

    Feat: Google 로그인 Response DTO 구현

--- src/main/java/com/donkeys_today/server/support/feign/dto/response/google/GoogleTokenResponse.java
+++ src/main/java/com/donkeys_today/server/support/feign/dto/response/google/GoogleTokenResponse.java
@@ -1,14 +1,28 @@
 package com.donkeys_today.server.support.feign.dto.response.google;
 
+import static com.donkeys_today.server.common.constants.Constants.BEARER;
+
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record GoogleTokenResponse(
-        String accessToken,
-        String refreshToken
+        String access_token,
+        String id_token
 ) {
-    public static GoogleTokenResponse of(String accessToken, String refreshToken) {
-        return new GoogleTokenResponse(accessToken, refreshToken);
+    public static GoogleTokenResponse of(String accessToken, String idToken) {
+        return new GoogleTokenResponse(accessToken, idToken);
+    }
+
+    @Override
+    public String toString() {
+        return "GoogleTokenResponse{" +
+                "access_token='" + access_token + '\'' +
+                ", id_token='" + id_token + '\'' +
+                '}';
+    }
+
+    public static String getTokenWithPrefix(String accessToken) {
+        return BEARER + accessToken;
     }
 }
