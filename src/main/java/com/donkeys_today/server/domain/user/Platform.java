0b2ba2d9b0088b86c9e0f945d81319cb27e5a37a
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-07-05 19:01:41 +0900

    Add: google 플랫폼 ENUM 추가

--- src/main/java/com/donkeys_today/server/domain/user/Platform.java
+++ src/main/java/com/donkeys_today/server/domain/user/Platform.java
@@ -8,16 +8,17 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
-public enum Platform{
+public enum Platform {
 
-  KAKAO("kakao");
+    KAKAO("kakao"),
+    GOOGLE("google");
 
-  private final String name;
+    private final String name;
 
-  public static Platform fromString(String name){
-    return Arrays.stream(Platform.values())
-        .filter(p -> p.name.equals(name))
-        .findFirst()
-        .orElseThrow(() -> new InternalServerException(ErrorType.INTERNAL_SERVER_ERROR));
-  }
+    public static Platform fromString(String name) {
+        return Arrays.stream(Platform.values())
+                .filter(p -> p.name.equals(name))
+                .findFirst()
+                .orElseThrow(() -> new InternalServerException(ErrorType.INTERNAL_SERVER_ERROR));
+    }
 }
