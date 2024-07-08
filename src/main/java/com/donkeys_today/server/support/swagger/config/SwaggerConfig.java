3c58ca04253653ada4c5f0410293c6870b0032f1
hyunw9 <rkdgusdnr32@naver.com>
2024-07-08 18:14:16 +0900

    Feat : Swagger Config 수정

--- src/main/java/com/donkeys_today/server/support/swagger/config/SwaggerConfig.java
+++ src/main/java/com/donkeys_today/server/support/swagger/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,10 +23,17 @@ public class SwaggerConfig {
           .scheme("bearer")
           .bearerFormat("JWT")
       );
+
+    Server server = new Server();
+    server.setUrl("https://clody.store");
+
     return new OpenAPI()
         .components(new Components())
+        .servers(List.of(server))
         .info(apiInfo());
   }
+
+
 
   private Info apiInfo(){
     return new Info()
