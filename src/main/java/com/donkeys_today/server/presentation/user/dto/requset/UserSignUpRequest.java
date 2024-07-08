74836bc08441e9d875976b9e225a14528547fe71
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-07-08 22:47:20 +0900

    Fix: UserSignUpRequest 수정

--- src/main/java/com/donkeys_today/server/presentation/user/dto/requset/UserSignUpRequest.java
+++ src/main/java/com/donkeys_today/server/presentation/user/dto/requset/UserSignUpRequest.java
@@ -1,10 +1,11 @@
 package com.donkeys_today.server.presentation.user.dto.requset;
 
-import java.time.LocalTime;
-
 public record UserSignUpRequest(
-    String platform,
-    boolean alarmAgreement,
-    LocalTime alarmTime
+        String platform,
+
+        String email,
+
+        String name
+
 ) {
 }
