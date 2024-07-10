2f551eae8875bed84b6c1b976771411c6ff50382
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-07-10 21:05:31 +0900

    Feat: DiaryInfo DTO 분리

---
+++ src/main/java/com/donkeys_today/server/presentation/Diary/dto/DiarySimpleInfo.java
@@ -0,0 +1,14 @@
+package com.donkeys_today.server.presentation.Diary.dto;
+
+
+import com.donkeys_today.server.domain.diary.ReplyStatus;
+
+public record DiarySimpleInfo(
+        int diaryCount,
+        ReplyStatus replyStatus
+
+) {
+    public static DiarySimpleInfo of(int diaryCount, ReplyStatus replyStatus) {
+        return new DiarySimpleInfo(diaryCount, replyStatus);
+    }
+}
