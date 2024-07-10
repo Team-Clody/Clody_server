cae830d046452e85bd6b98cac2ab9e499bb9707a
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-07-10 20:02:32 +0900

    Feat: userId, year, month 로 조회하는 쿼리 추가

--- src/main/java/com/donkeys_today/server/domain/diary/DiaryRepository.java
+++ src/main/java/com/donkeys_today/server/domain/diary/DiaryRepository.java
@@ -1,9 +1,15 @@
 package com.donkeys_today.server.domain.diary;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
-
+    @Query("SELECT d " +
+            "FROM Diary d WHERE d.user.id = :userId AND YEAR(d.createdAt) = :year AND MONTH(d.createdAt) = :month")
+    List<Diary> findContentsByUserIdAndYearAndMonth(@Param("userId") Long userId, @Param("year") int year,
+                                                    @Param("month") int month);
 }
--- src/main/java/com/donkeys_today/server/domain/reply/ReplyRepository.java
+++ src/main/java/com/donkeys_today/server/domain/reply/ReplyRepository.java
@@ -1,9 +1,14 @@
 package com.donkeys_today.server.domain.reply;
 
+import io.lettuce.core.dynamic.annotation.Param;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
-
+    @Query("SELECT r FROM Reply r WHERE r.user.id = :userId AND YEAR(r.createdDate) = :year AND MONTH(r.createdDate) = :month")
+    List<Reply> findRepliesByUserIdAndYearAndMonth(@Param("userId") Long userId, @Param("year") int year,
+                                                   @Param("month") int month);
 }
