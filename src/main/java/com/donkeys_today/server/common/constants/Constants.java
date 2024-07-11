d1a58912c8f0cfb63ea4eceaf7fc21fe3b872795
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-07-11 22:18:34 +0900

    Fix: RefreshToken Repository 함수 수정

--- src/main/java/com/donkeys_today/server/common/constants/Constants.java
+++ src/main/java/com/donkeys_today/server/common/constants/Constants.java
@@ -1,7 +1,9 @@
 package com.donkeys_today.server.common.constants;
 
 public abstract class Constants {
-  public static final String AUTHORIZATION = "Authorization";
-  public static final String BEARER = "Bearer ";
-  public static final String CHARACTER_TYPE = "utf-8";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+    public static final String CHARACTER_TYPE = "utf-8";
+    public static final String REFRESHTOKEN = "refreshToken";
+
 }
