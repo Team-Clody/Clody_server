a08533a2ec59faf7a6b5135a36d5922de3d7a101
hyunw9 <rkdgusdnr32@naver.com>
2024-07-11 13:49:43 +0900

    Feat : 알람 스케줄링 기능 구현

--- src/main/java/com/donkeys_today/server/application/alarm/AlarmScheduler.java
+++ src/main/java/com/donkeys_today/server/application/alarm/AlarmScheduler.java
@@ -4,24 +4,30 @@ import com.donkeys_today.server.domain.alarm.Alarm;
 import com.donkeys_today.server.external.fcm.FcmService;
 import com.donkeys_today.server.external.fcm.MessageContent;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class AlarmScheduler {
 
   private final FcmService fcmService;
   private final AlarmService alarmService;
 
-  @Scheduled(cron = "0 */1 * * * *")//Every 10 minutes
+  @Scheduled(cron = "0 */1 * * * *")//매 1분마다 실행
+  @Async
   public void checkAndSendAlarms(){
 
-    LocalTime currentTime = LocalTime.now();
+    LocalTime currentTime = LocalTime.now().withSecond(0).withNano(0);
     List<Alarm> alarmList = alarmService.findAlarmsByCurrentTime(currentTime);
+
+    log.info("Executing scheduled task at {}", currentTime);
+    log.info("alarmList Size : {}" , alarmList.size());
 
     for(Alarm userAlarm : alarmList){
       if(validateAlarm(userAlarm)){
