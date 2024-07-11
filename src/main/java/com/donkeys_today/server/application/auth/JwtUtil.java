6889d6770e03c96fb1b250630abce7391fdb68a1
hyunw9 <rkdgusdnr32@naver.com>
2024-07-11 13:50:56 +0900

    Feat : JwtUtil 추가

--- src/main/java/com/donkeys_today/server/application/auth/JwtUtil.java
+++ src/main/java/com/donkeys_today/server/application/auth/JwtUtil.java
@@ -3,15 +3,25 @@ package com.donkeys_today.server.application.auth;
 import com.donkeys_today.server.support.dto.type.ErrorType;
 import com.donkeys_today.server.support.exception.UnauthorizedException;
 import com.donkeys_today.server.support.security.auth.UserAuthentication;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class JwtUtil {
 
   public static Long getLoginMemberId() {
-    UserAuthentication authentication = (UserAuthentication) SecurityContextHolder.getContext()
-        .getAuthentication();
-    if (authentication != null) {
-      return (Long) authentication.getPrincipal();
+
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if (authentication != null && authentication.isAuthenticated()
+        && authentication instanceof UserAuthentication) {
+
+      Object principal = authentication.getPrincipal();
+      System.out.println((Long) principal);
+      if (principal instanceof Long) {
+        return (Long) principal;
+      } else if (principal instanceof String) {
+        return Long.parseLong((String) principal);
+      }
     }
     throw new UnauthorizedException(ErrorType.UNAUTHORIZED);
   }
