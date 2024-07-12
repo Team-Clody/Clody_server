549e06b98758cf35f2cdeff5edf0a99e5920f92c
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-07-12 15:10:10 +0900

    Refactor: UserController 의 인증 기능을 AuthController 로 분리시킴

---
+++ src/main/java/com/donkeys_today/server/presentation/user/AuthControllerImpl.java
@@ -0,0 +1,61 @@
+package com.donkeys_today.server.presentation.user;
+
+import com.donkeys_today.server.application.user.UserService;
+import com.donkeys_today.server.common.constants.Constants;
+import com.donkeys_today.server.presentation.api.AuthController;
+import com.donkeys_today.server.presentation.auth.dto.response.TokenReissueResponse;
+import com.donkeys_today.server.presentation.user.dto.requset.UserSignInRequest;
+import com.donkeys_today.server.presentation.user.dto.requset.UserSignUpRequest;
+import com.donkeys_today.server.presentation.user.dto.response.UserSignInResponse;
+import com.donkeys_today.server.presentation.user.dto.response.UserSignUpResponse;
+import com.donkeys_today.server.support.dto.ApiResponse;
+import com.donkeys_today.server.support.dto.type.SuccessType;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class AuthControllerImpl implements AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/auth/signup")
+    public ResponseEntity<ApiResponse<UserSignUpResponse>> signUp(
+            @RequestHeader(Constants.AUTHORIZATION) final String authorization_code,
+            @RequestBody final UserSignUpRequest userSignUpRequest) {
+        final UserSignUpResponse response = userService.signUp(authorization_code, userSignUpRequest);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success(SuccessType.CREATED_SUCCESS, response));
+    }
+
+    @ResponseBody
+    @PostMapping("/auth/signin")
+    public ResponseEntity<ApiResponse<UserSignInResponse>> signIn(
+            @RequestHeader(Constants.AUTHORIZATION) String authorization_code,
+            @RequestBody final UserSignInRequest userSignInRequest) {
+        final UserSignInResponse response = userService.signIn(authorization_code, userSignInRequest);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(SuccessType.OK_SUCCESS, response));
+    }
+
+    @Override
+    @GetMapping("/auth/reissue")
+    public ResponseEntity<ApiResponse<TokenReissueResponse>> reissue(HttpServletRequest request,
+                                                                     @CookieValue(name = Constants.REFRESHTOKEN) final String refreshToken) {
+        TokenReissueResponse response = userService.reissueAccessToken(refreshToken);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(SuccessType.OK_SUCCESS, response));
+
+    }
+}
