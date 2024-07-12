63c88b3c067f610ccb3ce493df9614b24b42de6b
hyunw9 <rkdgusdnr32@naver.com>
2024-07-12 15:42:32 +0900

    Feat : createdAt Instant -> LocalDateTime

--- src/main/java/com/donkeys_today/server/support/auditing/BaseEntity.java
+++ src/main/java/com/donkeys_today/server/support/auditing/BaseEntity.java
@@ -3,7 +3,7 @@ package com.donkeys_today.server.support.auditing;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import java.time.Instant;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -16,9 +16,9 @@ public class BaseEntity {
 
   @Column(name = "created_at", updatable = false)
   @CreatedDate
-  private Instant createdAt;
+  private LocalDateTime createdAt;
 
   @Column(name = "updated_at")
   @LastModifiedDate
-  private Instant updatedAt;
+  private LocalDateTime updatedAt;
 }
