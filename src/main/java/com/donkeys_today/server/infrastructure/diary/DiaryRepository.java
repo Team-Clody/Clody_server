08a9073d5ccad9cfce2199e33ab732eaad28f057
hyunw9 <rkdgusdnr32@naver.com>
2024-07-13 13:50:14 +0900

    Feat : Jpa Repository 구현

--- src/main/java/com/donkeys_today/server/infrastructure/diary/DiaryRepository.java
+++ src/main/java/com/donkeys_today/server/infrastructure/diary/DiaryRepository.java
@@ -1,10 +1,17 @@
 package com.donkeys_today.server.infrastructure.diary;
 
 import com.donkeys_today.server.domain.diary.Diary;
+import com.donkeys_today.server.domain.user.User;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+
+  boolean existsByUser(User user);
+
+  List<Diary> findDiariesByUserAndCreatedAtBetween(User user, LocalDateTime start, LocalDateTime end);
 
 }
