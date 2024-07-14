22ecc31d0c03570b9d282b99c4c2360e94ee7327
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-07-14 16:00:44 +0900

    Fix: 시간조회 DTO 수정

--- src/main/java/com/donkeys_today/server/presentation/Diary/dto/response/DiaryTimeResponse.java
+++ src/main/java/com/donkeys_today/server/presentation/Diary/dto/response/DiaryTimeResponse.java
@@ -3,11 +3,15 @@ package com.donkeys_today.server.presentation.Diary.dto.response;
 import java.time.LocalDate;
 
 public record DiaryTimeResponse(
-        LocalDate createdAt
+        LocalDate createdDate,
+        String HH,
+        String MM,
+        String SS
 
 ) {
-    public static DiaryTimeResponse of(LocalDate createdAt) {
-        return new DiaryTimeResponse(createdAt);
+    public static DiaryTimeResponse of(LocalDate createdDate, String hh, String mm, String ss) {
+
+        return new DiaryTimeResponse(createdDate, hh, mm, ss);
     }
 
 }
