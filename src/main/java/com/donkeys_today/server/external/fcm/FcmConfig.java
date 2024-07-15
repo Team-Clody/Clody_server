04d51f5cf4ca6936a127738e7e2eeafc7c441189
hyunw9 <rkdgusdnr32@naver.com>
2024-07-15 17:35:57 +0900

    Fix : 중복 클래스 수정

--- src/main/java/com/donkeys_today/server/external/fcm/FcmConfig.java
+++ src/main/java/com/donkeys_today/server/external/fcm/FcmConfig.java
@@ -4,25 +4,26 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class FcmConfig {
 
   @Bean
   public FirebaseApp firebaseApp() throws IOException {
-    FileInputStream aboutFirebaseFile = new FileInputStream(
-        String.valueOf(ResourceUtils.getFile("./secret/firebase-adminsdk.json")));
+    ClassPathResource resource = new ClassPathResource("firebase-adminsdk.json");
 
-    FirebaseOptions options = FirebaseOptions
-        .builder()
-        .setCredentials(GoogleCredentials.fromStream(aboutFirebaseFile))
-        .build();
-    return FirebaseApp.initializeApp(options);
+    try (InputStream aboutFirebaseFile = resource.getInputStream()) {
+      FirebaseOptions options = FirebaseOptions
+          .builder()
+          .setCredentials(GoogleCredentials.fromStream(aboutFirebaseFile))
+          .build();
+      return FirebaseApp.initializeApp(options);
+    }
   }
 
   @Bean
