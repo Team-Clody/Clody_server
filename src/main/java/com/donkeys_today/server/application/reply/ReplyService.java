0c9fd81b493f41ebec5c9c91acac46cd2d4378bc
hyunw9 <rkdgusdnr32@naver.com>
2024-07-16 17:04:31 +0900

    Feat : 답변 삭제 구현

--- src/main/java/com/donkeys_today/server/application/reply/ReplyService.java
+++ src/main/java/com/donkeys_today/server/application/reply/ReplyService.java
@@ -12,11 +12,20 @@ public class ReplyService {
 
   private final ReplyRetriever replyRetriever;
   private final ReplyUpdater replyUpdater;
+  private final ReplyRemover replyRemover;
 
   @Transactional
   public ReplyResponse readReply(String year, String month, String date) {
     Reply reply = replyRetriever.findReplyByDate(year, month, date);
     reply.readReply();
     return ReplyResponse.of(reply.getContent());
+  }
+
+  public boolean isReplyExist(Long userId, int year, int month, int date) {
+    return replyRetriever.isReplyExist(userId, year, month, date);
+  }
+
+  public void removeReply(Long userId, int year, int month, int date) {
+    replyRemover.removeReplyUserIdAndDate(userId, year, month, date);
   }
 }
