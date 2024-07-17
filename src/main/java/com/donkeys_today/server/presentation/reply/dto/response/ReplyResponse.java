f9420473494cd8f46d4246f5329be88b5ea87e70
hyunw9 <rkdgusdnr32@naver.com>
2024-07-17 20:09:44 +0900

    Feat : 답변에 월 / 일 추가

--- src/main/java/com/donkeys_today/server/presentation/reply/dto/response/ReplyResponse.java
+++ src/main/java/com/donkeys_today/server/presentation/reply/dto/response/ReplyResponse.java
@@ -2,9 +2,11 @@ package com.donkeys_today.server.presentation.reply.dto.response;
 
 public record ReplyResponse(
     String nickname,
-    String content
+    String content,
+    int month,
+    int date
 ) {
-  public static ReplyResponse of(String nickname, String content) {
-    return new ReplyResponse(nickname, content);
+  public static ReplyResponse of(String nickName, String content, int month, int date){
+    return new ReplyResponse(nickName, content, month, date);
   }
 }
