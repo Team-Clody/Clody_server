3115b902e31fc76b71148cdd1ae8f6bba3b5ae7a
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-07-17 19:06:15 +0900

    Docs: SignUpException 생성

--- src/main/java/com/donkeys_today/server/support/exception/GlobalExceptionAdvice.java
+++ src/main/java/com/donkeys_today/server/support/exception/GlobalExceptionAdvice.java
@@ -1,6 +1,7 @@
 package com.donkeys_today.server.support.exception;
 
 import com.donkeys_today.server.support.dto.ApiResponse;
+import com.donkeys_today.server.support.exception.auth.SignUpException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
 
-    @ExceptionHandler(BusinessException.class)
-    protected ResponseEntity<ApiResponse> handleNotFoundException(BusinessException e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiResponse.error(e.getErrorType()));
+    @ExceptionHandler(SignUpException.class)
+    protected ResponseEntity<ApiResponse> SignUpException(SignUpException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.error(e.getErrorType()));
     }
 }
