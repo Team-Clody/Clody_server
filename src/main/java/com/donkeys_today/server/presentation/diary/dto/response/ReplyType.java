76f611c9be31a22f2d683d77ab18273e914f0677
hyunw9 <rkdgusdnr32@naver.com>
2024-07-17 16:23:19 +0900

    Feat : 답변 타입 정의

---
+++ src/main/java/com/donkeys_today/server/presentation/diary/dto/response/ReplyType.java
@@ -0,0 +1,15 @@
+package com.donkeys_today.server.presentation.diary.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ReplyType {
+
+  STATIC_REPLY("정적 답변"),
+  DYNAMIC_REPLY("동적 답변"),
+  NO_REPLY("답변 없음");
+
+  private final String replyType;
+}
