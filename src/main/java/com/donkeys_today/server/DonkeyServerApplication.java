a787c127c79fb42dfe58211f71ed20cceab7c034
hyunw9 <rkdgusdnr32@naver.com>
2024-07-17 16:39:58 +0900

    Fix : propertySource 추가

--- src/main/java/com/donkeys_today/server/DonkeyServerApplication.java
+++ src/main/java/com/donkeys_today/server/DonkeyServerApplication.java
@@ -1,10 +1,13 @@
 package com.donkeys_today.server;
 
+import com.donkeys_today.server.support.config.ReplyProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 //@EnableScheduling
+@EnableConfigurationProperties(ReplyProperty.class)
 public class DonkeyServerApplication {
 
   public static void main(String[] args) {
