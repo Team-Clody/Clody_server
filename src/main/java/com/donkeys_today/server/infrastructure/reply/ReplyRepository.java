f22ec57fe16090a091cfe0f256b8b4a31f0ec0ee
hyunw9 <rkdgusdnr32@naver.com>
2024-07-18 00:12:31 +0900

    Docs : 중복 일기 예외 처리

--- src/main/java/com/donkeys_today/server/infrastructure/reply/ReplyRepository.java
+++ src/main/java/com/donkeys_today/server/infrastructure/reply/ReplyRepository.java
@@ -3,7 +3,6 @@ package com.donkeys_today.server.infrastructure.reply;
 import com.donkeys_today.server.domain.reply.Reply;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +11,7 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     List<Reply> findByUserIdAndDiaryCreatedDateBetween(Long userId, LocalDate start, LocalDate end);
 
-    Optional<Reply> findByUserIdAndDiaryCreatedDate(Long userId, LocalDate date);
+    List<Reply> findByUserIdAndDiaryCreatedDate(Long userId, LocalDate date);
 
     void deleteByUserIdAndDiaryCreatedDate(Long userId, LocalDate date);
 
