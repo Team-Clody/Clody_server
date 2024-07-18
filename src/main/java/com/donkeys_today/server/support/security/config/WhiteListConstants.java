7e80723c5d20af28556d4d9aa7dc49642f6efc54
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-07-18 21:44:33 +0900

    답장시간 30초, actuator 경로 open

--- src/main/java/com/donkeys_today/server/support/security/config/WhiteListConstants.java
+++ src/main/java/com/donkeys_today/server/support/security/config/WhiteListConstants.java
@@ -15,7 +15,7 @@ public class WhiteListConstants {
             "/api/v1/auth/signin",
             "/api/v1/auth/reissue",
 
-            "/actuator/health",
+            "/actuator/**",
             "/redirect",
             /*
             ** Swagger 관련 URL
