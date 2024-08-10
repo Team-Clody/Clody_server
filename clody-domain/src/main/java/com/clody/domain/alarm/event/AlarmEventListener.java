c4f564cc9f4f8fa41e3cbd49fc67cd7ef12361b5
hyunw9 <rkdgusdnr32@naver.com>
2024-08-10 20:29:55 +0900

    Feat : 알람 및 SQS 리스너 정의

--- clody-domain/src/main/java/com/clody/domain/alarm/event/AlarmEventListener.java
+++ clody-domain/src/main/java/com/clody/domain/alarm/event/AlarmEventListener.java
@@ -1,16 +1,24 @@
 package com.clody.domain.alarm.event;
 
+import com.clody.domain.alarm.Alarm;
+import com.clody.domain.alarm.repository.AlarmRepository;
+import com.clody.domain.user.User;
 import com.clody.domain.user.event.UserCreatedEvent;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class AlarmEventListener {
+
+  private final AlarmRepository alarmRepository;
 
   @EventListener
   public void handleUserCreatedEvent(UserCreatedEvent event){
-    //빈 알람 객체 생성 및 저장필요
-
+    //빈 알람 객체 생성 및 저장
+    User user = event.getUser();
+    Alarm alarm = Alarm.createInitailDiaryAlarm(user);
+    alarmRepository.save(alarm);
   }
-
 }
