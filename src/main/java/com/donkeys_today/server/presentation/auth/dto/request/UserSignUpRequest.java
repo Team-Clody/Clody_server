ed11d3d88613ed5822a3eb01c3012cec49af17af
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-08-16 23:58:45 +0900

    Refactor: 회원가입, 로그인 dto 수정

--- src/main/java/com/donkeys_today/server/presentation/auth/dto/request/UserSignUpRequest.java
+++ src/main/java/com/donkeys_today/server/presentation/auth/dto/request/UserSignUpRequest.java
@@ -6,8 +6,7 @@ public record UserSignUpRequest(
     @Schema(description = "로그인 플랫폼", example = "apple/kakao")
     String platform,
     String email,
-    String name,
-    @Schema(description = "애플 로그인", example = "ey23asdf.asdasdd.asdasd")
-    String id_token
+    String name
+
 ) {
 }
