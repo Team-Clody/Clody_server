0689f5494ebb834d4b214881b99cca3a3e515f1e
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-08-16 23:59:38 +0900

    Feat: 애플 로그인 구현

--- src/main/java/com/donkeys_today/server/support/feign/apple/AppleFeignClient.java
+++ src/main/java/com/donkeys_today/server/support/feign/apple/AppleFeignClient.java
@@ -4,7 +4,7 @@ import com.donkeys_today.server.support.feign.dto.response.apple.ApplePublicKeys
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "appleClient", url = "https://appleid.apple.com/auth", configuration = AppleFeignClientConfiguration.class)
+@FeignClient(name = "appleClient", url = "https://appleid.apple.com/auth")
 
 public interface AppleFeignClient {
     @GetMapping(value = "/keys")
