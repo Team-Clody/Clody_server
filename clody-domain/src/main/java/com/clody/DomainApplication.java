99e2cc7a28a40362815226132fd5ad981176b218
hyunw9 <rkdgusdnr32@naver.com>
2024-08-25 14:30:11 +0900

    Feat : Domain Application

--- clody-domain/src/main/java/com/clody/DomainApplication.java
+++ clody-domain/src/main/java/com/clody/DomainApplication.java
@@ -1,10 +1,14 @@
 package com.clody;
 
-//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-//public class DomainApplication {
-//
-//  public static void main(String[] args) {
-//    SpringApplication.run(DomainApplication.class, args);
-//  }
-//
-//}
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class DomainApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DomainApplication.class, args);
+  }
+
+}
