6e97da95537e41000c605face184fd5c80d167a5
hyunw9 <rkdgusdnr32@naver.com>
2024-08-26 23:57:18 +0900

    Feat(domain) : Message 에 Type 추가

--- clody-domain/src/main/java/com/clody/domain/reply/dto/Message.java
+++ clody-domain/src/main/java/com/clody/domain/reply/dto/Message.java
@@ -1,5 +1,6 @@
 package com.clody.domain.reply.dto;
 
+import com.clody.domain.reply.ReplyType;
 import com.clody.domain.reply.event.ReplyCreatedEvent;
 import java.time.LocalDateTime;
 
@@ -7,14 +8,15 @@ public record Message(
     Long replyId,
     String content,
     LocalDateTime creationTime,
-    Integer version
+    Integer version,
+    ReplyType type
 ) {
-  public static Message of(Long replyId, String content,Integer version) {
-    return new Message(replyId, content, LocalDateTime.now(), version);
+  public static Message of(Long replyId, String content,Integer version, ReplyType type) {
+    return new Message(replyId, content, LocalDateTime.now(), version,type);
   }
 
   public static Message of(ReplyCreatedEvent event){
-    return new Message(event.reply().getId(), event.parsedContent(), event.reply().getCreatedAt(), event.version());
+    return new Message(event.reply().getId(), event.parsedContent(), event.reply().getCreatedAt(), event.version(),event.reply().getReplyType());
   }
 
 }
