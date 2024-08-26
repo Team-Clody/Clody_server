d3d60ddf7141f9914cf34dde4099e9f13a1380d0
hyunw9 <rkdgusdnr32@naver.com>
2024-08-26 23:57:30 +0900

    Feat(support) : 메세지 시간 상수 추가

---
+++ clody-support/src/main/java/com/clody/support/constants/MessageProcessConstants.java
@@ -0,0 +1,12 @@
+package com.clody.support.constants;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class MessageProcessConstants {
+
+  public static final Long FIRST_DURATION = 60 * 1000L;
+  public static final Long DEFAULT_DURATION = 60 * 1000L * 10;
+  public static final Long STATIC_DURATION = 60 * 1000L * 5;
+
+}
