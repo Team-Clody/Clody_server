8e7e501d24efad14cc3fff64582ba7335d83a99a
hyunw9 <rkdgusdnr32@naver.com>
2024-08-30 01:42:29 +0900

    Feat(domain) : schedule 엔티티 구현

--- clody-domain/src/main/java/com/clody/meta/Schedule.java
+++ clody-domain/src/main/java/com/clody/meta/Schedule.java
@@ -9,11 +9,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@Getter
 @Table(name = "schedules")
 public class Schedule {
 
@@ -35,20 +38,22 @@ public class Schedule {
 
   @Builder
   public static Schedule create(Long userId, LocalDateTime replyCreationTime, ReplyType type) {
-    LocalDateTime notificationTime = calculateNotificationTime(type);
+
+    LocalDateTime notificationTime = calculateNotificationTime(replyCreationTime, type);
     return new Schedule(userId, notificationTime);
   }
 
-  private static LocalDateTime calculateNotificationTime(ReplyType type) {
-    switch (type) {
-      case FIRST:
-        return LocalDateTime.now().plusMinutes(10);
-      case STATIC:
-        return LocalDateTime.now().plusHours(5);
-      case DYNAMIC:
-        return LocalDateTime.now().plusHours(10);
-      default:
-        throw new BusinessException(ErrorType.INVALID_REPLY_TYPE);
-    }
+  public void notifySent() {
+    this.notificationSent = true;
+  }
+
+  private static LocalDateTime calculateNotificationTime(LocalDateTime replyCreationTime, ReplyType type) {
+    replyCreationTime = replyCreationTime.truncatedTo(ChronoUnit.SECONDS);
+    return switch (type) {
+      case FIRST -> replyCreationTime.plusMinutes(10);
+      case STATIC -> replyCreationTime.plusHours(5);
+      case DYNAMIC -> replyCreationTime.plusHours(10);
+      default -> throw new BusinessException(ErrorType.INVALID_REPLY_TYPE);
+    };
   }
 }
--- clody-domain/src/main/java/com/clody/meta/repository/ScheduleMetaRepository.java
+++ clody-domain/src/main/java/com/clody/meta/repository/ScheduleMetaRepository.java
@@ -1,8 +1,12 @@
 package com.clody.meta.repository;
 
 import com.clody.meta.Schedule;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ScheduleMetaRepository {
 
   void save(Schedule schedule);
+
+  List<Schedule> findSchedulesToNotify(LocalDateTime now);
 }
