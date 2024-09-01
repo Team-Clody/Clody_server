d5e04cb8bac1849e9739c5d147854c6ab50d445e
hyunw9 <rkdgusdnr32@naver.com>
2024-09-01 21:43:15 +0900

    Feat(infra) : MultiDataSource 설정 및 MultiTransactionManager 설정

--- clody-infra/src/main/java/com/clody/infra/config/DataSourceConfig.java
+++ clody-infra/src/main/java/com/clody/infra/config/DataSourceConfig.java
@@ -18,7 +18,15 @@ public class DataSourceConfig {
   @Bean
   @ConfigurationProperties(prefix = "spring.datasource.meta.hikari")
   public HikariConfig metaHikariConfig() {
-    return new HikariConfig();
+    HikariConfig config = new HikariConfig();
+
+    config.setDriverClassName("org.postgresql.Driver");
+    config.setJdbcUrl("jdbc:postgresql://localhost:5432/donkey_meta");
+    config.setUsername("hyunw9");
+    config.setPassword("1228");
+    System.out.println("DriverClassName: " + config.getDriverClassName());
+    System.out.println("JdbcUrl: " + config.getJdbcUrl());
+    return config;
   }
 
   @Primary
@@ -29,13 +37,18 @@ public class DataSourceConfig {
 
   @Bean
   @ConfigurationProperties(prefix = "spring.datasource.domain.hikari")
-  public HikariConfig businessHikariConfig() {
-    return new HikariConfig();
+  public HikariConfig domainHikariConfig() {
+    HikariConfig config = new HikariConfig();
+    config.setDriverClassName("org.postgresql.Driver");
+    config.setJdbcUrl("jdbc:postgresql://localhost:5432/donkey_local");
+    config.setUsername("hyunw9");
+    config.setPassword("1228");
+    return config;
   }
 
   @Bean(DOMAIN_DATASOURCE)
-  public DataSource businessDataSource(){
-    return new LazyConnectionDataSourceProxy(new HikariDataSource(businessHikariConfig()));
+  public DataSource domainDataSource(){
+    return new LazyConnectionDataSourceProxy(new HikariDataSource(domainHikariConfig()));
   }
 
 
