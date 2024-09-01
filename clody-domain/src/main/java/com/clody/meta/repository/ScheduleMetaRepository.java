a8cfcb20c32008706e457720fbf37b50ad35b809
hyunw9 <rkdgusdnr32@naver.com>
2024-09-01 22:04:07 +0900

    Chore : 컨벤션 및 변수명 수정

--- clody-domain/src/main/java/com/clody/meta/repository/ScheduleMetaRepository.java
+++ clody-domain/src/main/java/com/clody/meta/repository/ScheduleMetaRepository.java
@@ -3,8 +3,6 @@ package com.clody.meta.repository;
 import com.clody.meta.Schedule;
 import java.time.LocalDateTime;
 import java.util.List;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface ScheduleMetaRepository {
 
@@ -12,8 +10,7 @@ public interface ScheduleMetaRepository {
 
   List<Schedule> findSchedulesToNotify(LocalDateTime now);
 
-  Page<Schedule> findByIsSentFalseAndNotificationTimeLessThanEqual(LocalDateTime now, Pageable pageable);
+//  Page<Schedule> findByIsSentFalseAndNotificationTimeLessThanEqual(LocalDateTime now, Pageable pageable);
 
-  void saveAll(List<Schedule> scheduleList);
-
+//  void saveAll(List<Schedule> scheduleList);
 }
