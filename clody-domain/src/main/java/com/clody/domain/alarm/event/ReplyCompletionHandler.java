ac52fb3e3ec95ff1f3682c03bb99d45428b47f73
hyunw9 <rkdgusdnr32@naver.com>
2024-09-01 21:40:51 +0900

    Feat(domain) : alarm publisher 및 이벤트 정의

--- clody-domain/src/main/java/com/clody/domain/alarm/event/ReplyCompletionHandler.java
+++ clody-domain/src/main/java/com/clody/domain/alarm/event/ReplyCompletionHandler.java
@@ -1,8 +1,12 @@
 package com.clody.domain.alarm.event;
 
+import com.clody.domain.alarm.Alarm;
+import com.clody.domain.alarm.dto.ScheduleAlarmInfo;
 import com.clody.domain.alarm.repository.AlarmRepository;
-import com.clody.meta.repository.ScheduleMetaRepository;
-import com.clody.domain.alarm.service.NotificationSender;
+import com.clody.domain.alarm.service.AlarmScheduler;
+import com.clody.domain.alarm.strategy.ScheduleAlarmTimeFactory;
+import com.clody.domain.alarm.strategy.ScheduleTimeStrategy;
+import com.clody.domain.reply.dto.CreationMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -13,13 +17,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReplyCompletionHandler {
 
-  private final NotificationSender sender;
   private final AlarmRepository alarmRepository;
-  private final ScheduleMetaRepository scheduleMetaRepository;
+  private final ScheduleAlarmTimeFactory alarmTimeFactory;
+  private final AlarmScheduler alarmScheduler;
 
   @EventListener
-  public void handleReplyCompletion(ScheduleEvent event) {
-    scheduleMetaRepository.save(event.schedule());
-    log.info("알림 스케줄 정상 등록: {}", event.schedule());
+  public void handleReplyCompletion(CreationMessage event) {
+    /*
+    * 사용자가 알람 수신에 동의한 경우, 알림 스케줄에 등록
+     */
+    Alarm alarm = alarmRepository.findByUserId(event.userId());
+    alarm.validateUserAgreedForReplyAlarm();
+
+    ScheduleTimeStrategy scheduleTimeStrategy = alarmTimeFactory.getStrategy(event.type());
+    scheduleTimeStrategy.calculateAlarmDelayTime()
+        .map(rt -> ScheduleAlarmInfo.of(event, rt, alarm.getFcmToken()))
+        .ifPresent(info -> {
+          alarmScheduler.scheduleReplyPushAlarm(info);
+          log.info("알림 스케줄 정상 등록: {}", info);
+        });
   }
 }
