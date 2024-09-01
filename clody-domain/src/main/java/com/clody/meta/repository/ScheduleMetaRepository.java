d19185c6c6a9a20d553a4881c44f8d1bdc39adeb
hyunw9 <rkdgusdnr32@naver.com>
2024-09-01 21:36:21 +0900

    Feat(domain) : Schedule 및 Repository

--- clody-domain/src/main/java/com/clody/meta/repository/ScheduleMetaRepository.java
+++ clody-domain/src/main/java/com/clody/meta/repository/ScheduleMetaRepository.java
@@ -3,10 +3,17 @@ package com.clody.meta.repository;
 import com.clody.meta.Schedule;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ScheduleMetaRepository {
 
   void save(Schedule schedule);
 
   List<Schedule> findSchedulesToNotify(LocalDateTime now);
+
+  Page<Schedule> findByIsSentFalseAndNotificationTimeLessThanEqual(LocalDateTime now, Pageable pageable);
+
+  void saveAll(List<Schedule> scheduleList);
+
 }
