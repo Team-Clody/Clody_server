735ce108a8c222dbc4594ff89fc66a2be601f7e5
hyunw9 <rkdgusdnr32@naver.com>
2024-09-04 15:19:35 +0900

    Feat : 테스트 및 시간 고정을 위한 Clock 객체 주입

---
+++ clody-infra/src/main/java/com/clody/infra/config/TimeConfig.java
@@ -0,0 +1,15 @@
+package com.clody.infra.config;
+
+import java.time.Clock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TimeConfig {
+
+  @Bean
+  public Clock clock(){
+    return Clock.systemDefaultZone();
+  }
+
+}
