b99510be45e62ec2443518d979d6341f78d918cc
강현욱 <43662405+hyunw9@users.noreply.github.com>
2024-09-04 15:21:02 +0900

    Feat/#189: 10분단위 스케줄링 구현 (#190)
    * Feat(infra) : 10분단위 스케줄링 구현
    
    Spring Scheduler + Cron tab을 이용하여 구현하였습니다.
    
    * Feat : 테스트 및 시간 고정을 위한 Clock 객체 주입
    
    * Test : 스케줄러 단위테스트
    
    * Feat : Service 시간 주입 방식 변경
    
    * Fix : Schedule 객체 사소한 수정

---
+++ clody-infra/src/main/java/com/clody/infra/models/alarm/DiaryNotifyService.java
@@ -0,0 +1,38 @@
+package com.clody.infra.models.alarm;
+
+import com.clody.domain.alarm.Alarm;
+import com.clody.domain.alarm.repository.AlarmRepository;
+import com.clody.infra.external.fcm.FcmService;
+import java.time.Clock;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DiaryNotifyService {
+
+  private final AlarmRepository alarmRepository;
+  private final FcmService fcmService;
+  private final Clock clock;
+
+  @Scheduled(cron = "0 0/10 * * * *")
+  public void sendDiaryAlarms() {
+    LocalTime time = LocalTime.now(clock).truncatedTo(ChronoUnit.SECONDS);
+
+    List<Alarm> alarmTarget = alarmRepository.findAllByTime(time);
+
+    alarmTarget.stream()
+        .filter(Alarm::isDiaryAlarm)
+        .forEach(
+            it -> fcmService.sendDiaryAlarm(it.getFcmToken())
+        );
+    log.info("Alarm Successfully Sent");
+  }
+
+}
