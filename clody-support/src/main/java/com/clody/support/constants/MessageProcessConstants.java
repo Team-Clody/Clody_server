f9c21f213b2d3f1cdb4cf85d3b2c714983163a76
강현욱 <43662405+hyunw9@users.noreply.github.com>
2024-09-05 11:43:31 +0900

    Fix : 첫 요청 응답시간 수정  (#191)
    * Feat(infra) : 10분단위 스케줄링 구현
    
    Spring Scheduler + Cron tab을 이용하여 구현하였습니다.
    
    * Feat : 테스트 및 시간 고정을 위한 Clock 객체 주입
    
    * Test : 스케줄러 단위테스트
    
    * Feat : Service 시간 주입 방식 변경
    
    * Fix : Schedule 객체 사소한 수정
    
    * Fix : Message Processing Time

--- clody-support/src/main/java/com/clody/support/constants/MessageProcessConstants.java
+++ clody-support/src/main/java/com/clody/support/constants/MessageProcessConstants.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class MessageProcessConstants {
 
-  public static final Long FIRST_DURATION = 10 * 60L ;
+  public static final Long FIRST_DURATION = 1 * 60L ;
   public static final Long DEFAULT_DURATION = 2* 60 * 60L;
   public static final Long STATIC_DURATION = 2 * 60 * 60L;
 
