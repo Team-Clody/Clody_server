d599aef09c9c7b278cd41d5555ed9bde3de2a91b
hyunw9 <rkdgusdnr32@naver.com>
2024-09-05 11:42:06 +0900

    Fix : Message Processing Time

--- clody-support/src/main/java/com/clody/support/constants/MessageProcessConstants.java
+++ clody-support/src/main/java/com/clody/support/constants/MessageProcessConstants.java
@@ -5,8 +5,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class MessageProcessConstants {
 
-  public static final Long FIRST_DURATION = 60L ;
-  public static final Long DEFAULT_DURATION = 60 * 5L;
-  public static final Long STATIC_DURATION = 60 * 5L;
+  public static final Long FIRST_DURATION = 1 * 60L ;
+  public static final Long DEFAULT_DURATION = 2* 60 * 60L;
+  public static final Long STATIC_DURATION = 2 * 60 * 60L;
 
 }
