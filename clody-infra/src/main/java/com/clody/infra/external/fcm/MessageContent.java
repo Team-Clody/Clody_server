df6020a31b7569b2b0b284fbb3f2652427c35be8
강현욱 <43662405+hyunw9@users.noreply.github.com>
2024-09-07 01:04:56 +0900

    Fix/#197 : 푸시알림 메세지를 수정합니다.  (#200)
    * Fix : 처리 실패시 예외 발생
    
    * Fix : 하루 범위 조회 로직 수정
    
    * Fix : 푸시알림 메세지 수정

--- clody-infra/src/main/java/com/clody/infra/external/fcm/MessageContent.java
+++ clody-infra/src/main/java/com/clody/infra/external/fcm/MessageContent.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @Getter
 public enum MessageContent {
 
-  DIARY_WRITE_REQUEST("클로디","오늘의 일기를 작성해주세요"),
-  REPLY_COMPLETED_REQUEST("클로디","답변 도착 ! 확인해보세요 "),
+  DIARY_WRITE_REQUEST("클로디","오늘의 감사한 일을 말해보세요!"),
+  REPLY_COMPLETED_REQUEST("클로디","행운의 답장이 도착했어요!"),
   ;
   public final String title;
   public final String body;
